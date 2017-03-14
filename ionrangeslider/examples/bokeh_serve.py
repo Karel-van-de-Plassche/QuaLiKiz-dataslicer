f52c9a520b8084f1ebb13f8f74c45e70daab2f85
Karel-van-de-Plassche <karelvandeplassche@gmail.com>
2017-03-14 17:17:15 +0100

    Added comparison example and extented for ionrangeslider

--- ionrangeslider/examples/bokeh_serve.py
+++ ionrangeslider/examples/bokeh_serve.py
@@ -14,8 +14,7 @@ y = x
 source = ColumnDataSource(data=dict(x=x, y=y))
 
 plot = Figure(plot_width=400, plot_height=400)
-plot.line('x', 'y', source=source,
-          line_width=3, line_alpha=0.6, color='#ed5565')
+plot.line('x', 'y', source=source, line_width=3, line_alpha=0.6, color='#ed5565')
 
 callback_single = CustomJS(args=dict(source=source), code="""
         var data = source.data;
@@ -43,14 +42,25 @@ callback_ion = CustomJS(args=dict(source=source), code="""
         source.trigger('change');
     """)
 
-code = CustomJS(code="""
-         var f = cb_obj
-         f = Number(f.toPrecision(2))
-         return f
-     """)
+prettifier = CustomJS(code="""
+        var f = cb_obj
+        f = Number(f.toPrecision(2))
+        return f
+    """)
+# Native bokeh slider
 slider = Slider(start=0, end=5, step=0.1, value=1, title="Bokeh Slider - Power", callback=callback_single)
-ion_range_slider = IonRangeSlider(values=list(range(10)), title='Ion Range Slider - Range', callback=callback_ion, callback_policy='continuous')
-#ion_range_slider2 = IonRangeSlider(values=[0, 1e-4, 0.00999993], title='Test Slider', prettify_enabled=True, prettify=code)
-
-layout = column(plot, slider, ion_range_slider)
+# IonRangeSlider from http://bokeh.pydata.org/en/latest/docs/user_guide/extensions_gallery/widget.html
+ion_range_slider = IonRangeSlider(start=0.01, end=0.99, step=0.01,
+                                  range=(min(x), max(x)),
+                                  title='Ion Range Slider - Range',
+                                  callback=callback_ion,
+                                  callback_policy='continuous',
+                                  slider_type='double')
+# IonRangeSlider with extended capabilities
+ion_range_slider2 = IonRangeSlider(values=[0, 1e-4, 0.00999993],
+                                   title='Extended Slider',
+                                   prettify_enabled=True, prettify=prettifier,
+                                   grid=False, color='green',
+                                   force_edges=True)
+layout = column(plot, slider, ion_range_slider, ion_range_slider2)
 curdoc().add_root(layout)
