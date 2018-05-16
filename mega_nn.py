6ac9964b3af088a56f2641c0a073f026453e04ce
Karel-van-de-Plassche <karelvandeplassche@gmail.com>
2018-05-16 13:43:02 +0200

    Changed to RAPTOR-style network selection

--- mega_nn.py
+++ mega_nn.py
@@ -5,20 +5,44 @@ import pandas as pd
 import numpy as np
 from IPython import embed
 
-from qlknn.NNDB.model import Network
+from qlknn.NNDB.model import Network, select_from_candidate_query, get_pure_from_cost_l2_scale, get_from_cost_l2_scale_array
 from qlknn.models.ffnn import QuaLiKizNDNN, QuaLiKizComboNN
 from qlknn.models.clipping import LeadingFluxNN
 from qlknn.models.victor_rule import VictorNN
 
-ITG = Network.get_by_id(890).to_QuaLiKizNN() #5e-5
-TEM = Network.get_by_id(1049).to_QuaLiKizNN() #5e-5
-ETG = Network.get_by_id(740).to_QuaLiKizNN() #5e-5
-gam = Network.get_by_id(887).to_QuaLiKizNN() #1e-5
+
+ITG_list = [get_pure_from_cost_l2_scale('efiITG_GB', 5e-5)]
+ITG_list.append(get_from_cost_l2_scale_array('efeITG_GB', '{5e-5}'))
+ITG_list.append(get_from_cost_l2_scale_array('pfeITG_GB', '{5e-5}'))
+ITG_list.append(get_from_cost_l2_scale_array('dfeITG_GB', '{5e-5, 8e-5}'))
+ITG_list.append(get_from_cost_l2_scale_array('dfiITG_GB', '{5e-5, 8e-5}'))
+ITG_list.append(get_from_cost_l2_scale_array('vteITG_GB', '{5e-5, 8e-5}'))
+ITG_list.append(get_from_cost_l2_scale_array('vtiITG_GB', '{5e-5, 8e-5}'))
+ITG_list.append(get_from_cost_l2_scale_array('vceITG_GB', '{5e-5, 8e-5}'))
+ITG_list.append(get_from_cost_l2_scale_array('vciITG_GB', '{5e-5, 8e-5}'))
+TEM_list = [get_pure_from_cost_l2_scale('efeTEM_GB', 5e-5)]
+TEM_list.append(get_from_cost_l2_scale_array('efiTEM_GB', '{5e-5}'))
+TEM_list.append(get_from_cost_l2_scale_array('pfeTEM_GB', '{5e-5, 1e-4}'))
+TEM_list.append(get_from_cost_l2_scale_array('dfeTEM_GB', '{5e-5, 8e-5}'))
+TEM_list.append(get_from_cost_l2_scale_array('dfiTEM_GB', '{5e-5, 8e-5}'))
+TEM_list.append(get_from_cost_l2_scale_array('vteTEM_GB', '{5e-5, 8e-5}'))
+TEM_list.append(get_from_cost_l2_scale_array('vtiTEM_GB', '{5e-5, 8e-5}'))
+TEM_list.append(get_from_cost_l2_scale_array('vceTEM_GB', '{5e-5, 8e-5}'))
+TEM_list.append(get_from_cost_l2_scale_array('vciTEM_GB', '{5e-5, 8e-5}'))
+ETG_list = [get_pure_from_cost_l2_scale('efeETG_GB', 5e-5)]
+Network_list = ITG_list + TEM_list + ETG_list
+#{gam_leq_GB}
+netgam = get_pure_from_cost_l2_scale('gam_leq_GB', 1e-5)
+
+gam = netgam.to_QuaLiKizNN() #1e-5
+nets = [net.to_QuaLiKizNN() for net in Network_list]
+combo_target_names = []
+for net in Network_list:
+    combo_target_names.extend(net.target_names)
 def combo_func(*args):
     return np.hstack(args)
-combo_target_names = ITG._target_names.append(TEM._target_names, ignore_index=True).append(ETG._target_names, ignore_index=True)
 #.append(gam._target_names, ignore_index=True)
-combo_nn = QuaLiKizComboNN(combo_target_names, [ITG, TEM, ETG], combo_func)
+combo_nn = QuaLiKizComboNN(pd.Series(combo_target_names), nets, combo_func)
 vic_nn = VictorNN(combo_nn, gam)
 nn = LeadingFluxNN.add_leading_flux_clipping(vic_nn)
 
@@ -39,10 +63,10 @@ if __name__ == '__main__':
     low_bound = None
     high_bound = None
 
-    print('Seperate')
-    print(ITG.get_output(input, clip_high=False, clip_low=False, high_bound=high_bound, low_bound=low_bound))
-    print(TEM.get_output(input, clip_high=False, clip_low=False, high_bound=high_bound, low_bound=low_bound))
-    print(ETG.get_output(input, clip_high=False, clip_low=False, high_bound=high_bound, low_bound=low_bound))
+    #print('Seperate')
+    #print(ITG.get_output(input, clip_high=False, clip_low=False, high_bound=high_bound, low_bound=low_bound))
+    #print(TEM.get_output(input, clip_high=False, clip_low=False, high_bound=high_bound, low_bound=low_bound))
+    #print(ETG.get_output(input, clip_high=False, clip_low=False, high_bound=high_bound, low_bound=low_bound))
     print('Combo NN')
     print(combo_nn.get_output(input, clip_high=False, clip_low=False, high_bound=high_bound, low_bound=low_bound))
     input['gammaE'] = np.full_like(input['Ati'], 0.1)
