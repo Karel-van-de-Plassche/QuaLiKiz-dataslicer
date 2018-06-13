0a67ee7f19c1b199febf15db1ed6fc2d4140818b
Karel-van-de-Plassche <karelvandeplassche@gmail.com>
2018-06-13 09:45:57 +0200

    Now include gen3 RAPTOR nns in dataslicer

--- mega_nn.py
+++ mega_nn.py
@@ -11,37 +11,51 @@ from qlknn.models.clipping import LeadingFluxNN
 from qlknn.models.victor_rule import VictorNN
 
 
-ITG_list = [get_pure_from_cost_l2_scale('efiITG_GB', 5e-5)]
-ITG_list.append(get_from_cost_l2_scale_array('efeITG_GB', '{5e-5}'))
-ITG_list.append(get_from_cost_l2_scale_array('pfeITG_GB', '{5e-5}'))
-ITG_list.append(get_from_cost_l2_scale_array('dfeITG_GB', '{5e-5, 8e-5}'))
-ITG_list.append(get_from_cost_l2_scale_array('dfiITG_GB', '{5e-5, 8e-5}'))
-ITG_list.append(get_from_cost_l2_scale_array('vteITG_GB', '{5e-5, 8e-5}'))
-ITG_list.append(get_from_cost_l2_scale_array('vtiITG_GB', '{5e-5, 8e-5}'))
-ITG_list.append(get_from_cost_l2_scale_array('vceITG_GB', '{5e-5, 8e-5}'))
-ITG_list.append(get_from_cost_l2_scale_array('vciITG_GB', '{5e-5, 8e-5}'))
-TEM_list = [get_pure_from_cost_l2_scale('efeTEM_GB', 5e-5)]
-TEM_list.append(get_from_cost_l2_scale_array('efiTEM_GB', '{5e-5}'))
-TEM_list.append(get_from_cost_l2_scale_array('pfeTEM_GB', '{5e-5, 1e-4}'))
-TEM_list.append(get_from_cost_l2_scale_array('dfeTEM_GB', '{5e-5, 8e-5}'))
-TEM_list.append(get_from_cost_l2_scale_array('dfiTEM_GB', '{5e-5, 8e-5}'))
-TEM_list.append(get_from_cost_l2_scale_array('vteTEM_GB', '{5e-5, 8e-5}'))
-TEM_list.append(get_from_cost_l2_scale_array('vtiTEM_GB', '{5e-5, 8e-5}'))
-TEM_list.append(get_from_cost_l2_scale_array('vceTEM_GB', '{5e-5, 8e-5}'))
-TEM_list.append(get_from_cost_l2_scale_array('vciTEM_GB', '{5e-5, 8e-5}'))
-ETG_list = [get_pure_from_cost_l2_scale('efeETG_GB', 5e-5)]
-Network_list = ITG_list + TEM_list + ETG_list
-#{gam_leq_GB}
-netgam = get_pure_from_cost_l2_scale('gam_leq_GB', 1e-5)
-
-gam = netgam.to_QuaLiKizNN() #1e-5
-nets = [net.to_QuaLiKizNN() for net in Network_list]
-combo_target_names = []
-for net in Network_list:
-    combo_target_names.extend(net.target_names)
 def combo_func(*args):
     return np.hstack(args)
-#.append(gam._target_names, ignore_index=True)
+
+nn_source = 'NNDB'
+nn_source = 'RAPTOR_gen3_nets'
+if nn_source == 'NNDB':
+    ITG_list = [get_pure_from_cost_l2_scale('efiITG_GB', 5e-5)]
+    ITG_list.append(get_from_cost_l2_scale_array('efeITG_GB', '{5e-5}'))
+    ITG_list.append(get_from_cost_l2_scale_array('pfeITG_GB', '{5e-5}'))
+    ITG_list.append(get_from_cost_l2_scale_array('dfeITG_GB', '{5e-5, 8e-5}'))
+    ITG_list.append(get_from_cost_l2_scale_array('dfiITG_GB', '{5e-5, 8e-5}'))
+    ITG_list.append(get_from_cost_l2_scale_array('vteITG_GB', '{5e-5, 8e-5}'))
+    ITG_list.append(get_from_cost_l2_scale_array('vtiITG_GB', '{5e-5, 8e-5}'))
+    ITG_list.append(get_from_cost_l2_scale_array('vceITG_GB', '{5e-5, 8e-5}'))
+    ITG_list.append(get_from_cost_l2_scale_array('vciITG_GB', '{5e-5, 8e-5}'))
+    TEM_list = [get_pure_from_cost_l2_scale('efeTEM_GB', 5e-5)]
+    TEM_list.append(get_from_cost_l2_scale_array('efiTEM_GB', '{5e-5}'))
+    TEM_list.append(get_from_cost_l2_scale_array('pfeTEM_GB', '{5e-5, 1e-4}'))
+    TEM_list.append(get_from_cost_l2_scale_array('dfeTEM_GB', '{5e-5, 8e-5}'))
+    TEM_list.append(get_from_cost_l2_scale_array('dfiTEM_GB', '{5e-5, 8e-5}'))
+    TEM_list.append(get_from_cost_l2_scale_array('vteTEM_GB', '{5e-5, 8e-5}'))
+    TEM_list.append(get_from_cost_l2_scale_array('vtiTEM_GB', '{5e-5, 8e-5}'))
+    TEM_list.append(get_from_cost_l2_scale_array('vceTEM_GB', '{5e-5, 8e-5}'))
+    TEM_list.append(get_from_cost_l2_scale_array('vciTEM_GB', '{5e-5, 8e-5}'))
+    ETG_list = [get_pure_from_cost_l2_scale('efeETG_GB', 5e-5)]
+    Network_list = ITG_list + TEM_list + ETG_list
+    #{gam_leq_GB}
+    netgam = get_pure_from_cost_l2_scale('gam_leq_GB', 1e-5)
+
+    gam = netgam.to_QuaLiKizNN() #1e-5
+    nets = [net.to_QuaLiKizNN() for net in Network_list]
+    combo_target_names = []
+    for net in Network_list:
+        combo_target_names.extend(net.target_names)
+    #.append(gam._target_names, ignore_index=True)
+elif nn_source == 'RAPTOR_gen3_nets':
+    from collections import OrderedDict
+    networks = OrderedDict()
+    for path in os.listdir(nn_source):
+        nn = QuaLiKizNDNN.from_json(os.path.join(nn_source, path + '/nn.json'))
+        networks[tuple(nn._target_names)] = nn
+    gam = networks.pop(('gam_leq_GB',))
+    nets = list(networks.values())
+    combo_target_names = [key[0] for key in networks.keys()]
+
 combo_nn = QuaLiKizComboNN(pd.Series(combo_target_names), nets, combo_func)
 vic_nn = VictorNN(combo_nn, gam)
 nn = LeadingFluxNN.add_leading_flux_clipping(vic_nn)
