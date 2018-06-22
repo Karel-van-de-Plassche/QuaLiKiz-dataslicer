88c6770c183d31fec6b92d199a06bb89d589b1ac
Karel-van-de-Plassche <karelvandeplassche@gmail.com>
2018-06-22 14:46:35 +0200

    Added RAPTOR-style networks from NNDB

--- mega_nn.py
+++ mega_nn.py
@@ -5,7 +5,7 @@ import pandas as pd
 import numpy as np
 from IPython import embed
 
-from qlknn.NNDB.model import Network, select_from_candidate_query, get_pure_from_cost_l2_scale, get_from_cost_l2_scale_array
+from qlknn.NNDB.model import Network, select_from_candidate_query, get_pure_from_cost_l2_scale, get_from_cost_l2_scale_array, get_pure_from_hyperpar
 from qlknn.models.ffnn import QuaLiKizNDNN, QuaLiKizComboNN
 from qlknn.models.clipping import LeadingFluxNN
 from qlknn.models.victor_rule import VictorNN
@@ -16,36 +16,60 @@ def combo_func(*args):
     return np.hstack(args)
 
 nn_source = 'NNDB'
-nn_source = 'RAPTOR_gen3_nets'
+#nn_source = 'RAPTOR_gen3_nets'
 if nn_source == 'NNDB':
-    ITG_list = [get_pure_from_cost_l2_scale('efiITG_GB', 5e-5)]
-    ITG_list.append(get_from_cost_l2_scale_array('efeITG_GB', '{5e-5}'))
-    ITG_list.append(get_from_cost_l2_scale_array('pfeITG_GB', '{5e-5}'))
-    ITG_list.append(get_from_cost_l2_scale_array('dfeITG_GB', '{5e-5, 8e-5}'))
-    ITG_list.append(get_from_cost_l2_scale_array('dfiITG_GB', '{5e-5, 8e-5}'))
-    ITG_list.append(get_from_cost_l2_scale_array('vteITG_GB', '{5e-5, 8e-5}'))
-    ITG_list.append(get_from_cost_l2_scale_array('vtiITG_GB', '{5e-5, 8e-5}'))
-    ITG_list.append(get_from_cost_l2_scale_array('vceITG_GB', '{5e-5, 8e-5}'))
-    ITG_list.append(get_from_cost_l2_scale_array('vciITG_GB', '{5e-5, 8e-5}'))
-    TEM_list = [get_pure_from_cost_l2_scale('efeTEM_GB', 5e-5)]
-    TEM_list.append(get_from_cost_l2_scale_array('efiTEM_GB', '{5e-5}'))
-    TEM_list.append(get_from_cost_l2_scale_array('pfeTEM_GB', '{5e-5, 1e-4}'))
-    TEM_list.append(get_from_cost_l2_scale_array('dfeTEM_GB', '{5e-5, 8e-5}'))
-    TEM_list.append(get_from_cost_l2_scale_array('dfiTEM_GB', '{5e-5, 8e-5}'))
-    TEM_list.append(get_from_cost_l2_scale_array('vteTEM_GB', '{5e-5, 8e-5}'))
-    TEM_list.append(get_from_cost_l2_scale_array('vtiTEM_GB', '{5e-5, 8e-5}'))
-    TEM_list.append(get_from_cost_l2_scale_array('vceTEM_GB', '{5e-5, 8e-5}'))
-    TEM_list.append(get_from_cost_l2_scale_array('vciTEM_GB', '{5e-5, 8e-5}'))
-    ETG_list = [get_pure_from_cost_l2_scale('efeETG_GB', 5e-5)]
-    Network_list = ITG_list + TEM_list + ETG_list
-    #{gam_leq_GB}
-    netgam = get_pure_from_cost_l2_scale('gam_leq_GB', 1e-5)
+    nN_mn_out = 7
 
-    gam = netgam.to_QuaLiKizNN() #1e-5
-    nets = [net.to_QuaLiKizNN() for net in Network_list]
+    ITG_list = [get_pure_from_hyperpar(
+        'efiITG_GB', nN_mn_out,
+        cost_l2_scale=5e-5,
+        cost_stable_positive_function='block',
+        cost_stable_positive_scale=1e-3,
+        cost_stable_positive_offset=-5)
+                ]
+    ITG_list.append(get_pure_from_hyperpar('efeITG_GB_div_efiITG_GB', nN_mn_out, cost_l2_scale=5e-5))
+    ITG_list.append(get_pure_from_hyperpar('pfeITG_GB_div_efiITG_GB', nN_mn_out, cost_l2_scale=5e-5))
+    ITG_list.append(get_pure_from_hyperpar('dfeITG_GB_div_efiITG_GB', nN_mn_out, cost_l2_scale=5e-5))
+    ITG_list.append(get_pure_from_hyperpar('dfiITG_GB_div_efiITG_GB', nN_mn_out, cost_l2_scale=5e-5))
+    ITG_list.append(get_pure_from_hyperpar('vteITG_GB_div_efiITG_GB', nN_mn_out, cost_l2_scale=5e-5))
+    ITG_list.append(get_pure_from_hyperpar('vtiITG_GB_div_efiITG_GB', nN_mn_out, cost_l2_scale=5e-5))
+    ITG_list.append(get_pure_from_hyperpar('vceITG_GB_div_efiITG_GB', nN_mn_out, cost_l2_scale=5e-5))
+    ITG_list.append(get_pure_from_hyperpar('vciITG_GB_div_efiITG_GB', nN_mn_out, cost_l2_scale=5e-5))
+    TEM_list = [get_pure_from_hyperpar(
+        'efeTEM_GB', nN_mn_out,
+        cost_l2_scale=5e-5,
+        cost_stable_positive_function='block',
+        cost_stable_positive_scale=1e-3,
+        cost_stable_positive_offset=-5)
+                ]
+    TEM_list.append(get_pure_from_hyperpar('efiTEM_GB_div_efeTEM_GB', nN_mn_out, cost_l2_scale=5e-5))
+    TEM_list.append(get_pure_from_hyperpar('pfeTEM_GB_div_efeTEM_GB', nN_mn_out, cost_l2_scale=5e-5))
+    TEM_list.append(get_pure_from_hyperpar('dfeTEM_GB_div_efeTEM_GB', nN_mn_out, cost_l2_scale=5e-5))
+    TEM_list.append(get_pure_from_hyperpar('dfiTEM_GB_div_efeTEM_GB', nN_mn_out, cost_l2_scale=5e-5))
+    TEM_list.append(get_pure_from_hyperpar('vteTEM_GB_div_efeTEM_GB', nN_mn_out, cost_l2_scale=5e-5))
+    TEM_list.append(get_pure_from_hyperpar('vtiTEM_GB_div_efeTEM_GB', nN_mn_out, cost_l2_scale=5e-5))
+    TEM_list.append(get_pure_from_hyperpar('vceTEM_GB_div_efeTEM_GB', nN_mn_out, cost_l2_scale=5e-5))
+    TEM_list.append(get_pure_from_hyperpar('vciTEM_GB_div_efeTEM_GB', nN_mn_out, cost_l2_scale=5e-5))
+    ETG_list = [get_pure_from_hyperpar(
+        'efeETG_GB', nN_mn_out,
+        cost_l2_scale=5e-5,
+        cost_stable_positive_function='block',
+        cost_stable_positive_scale=1e-3,
+        cost_stable_positive_offset=-5)
+                ]
+    netgam = get_pure_from_hyperpar('gam_leq_GB', nN_mn_out, cost_l2_scale=2e-5)
+
+
+    Network_list = ITG_list + TEM_list + ETG_list
+    Network_list.append(netgam)
+    #{gam_leq_GB}
+
     combo_target_names = []
-    for net in Network_list:
-        combo_target_names.extend(net.target_names)
+    networks = {net.target_names[0]: net.to_QuaLiKizNDNN() for net in Network_list}
+
+
+    #for net in Network_list:
+    #    combo_target_names.extend(net.target_names)
     #.append(gam._target_names, ignore_index=True)
 elif nn_source == 'RAPTOR_gen3_nets':
     from collections import OrderedDict
@@ -56,20 +80,21 @@ elif nn_source == 'RAPTOR_gen3_nets':
             raise
         else:
             networks[nn._target_names[0]] = nn
-    # Match all div networks with their leading fluxes
-    for target_name in list(networks.keys()):
-        if is_transport(target_name) and not is_pure(target_name):
-            target, op, leading = split_parts(target_name)
-            if op != '_div_':
-                raise
-            nn = QuaLiKizComboNN(pd.Series(target),
-                                 [networks.pop(target_name), networks[leading]],
-                                 lambda x, y: x * y)
-            networks[target] = nn
 
-    gam = networks.pop('gam_leq_GB')
-    nets = list(networks.values())
-    combo_target_names = list(networks.keys())
+# Match all div networks with their leading fluxes
+for target_name in list(networks.keys()):
+    if is_transport(target_name) and not is_pure(target_name):
+        target, op, leading = split_parts(target_name)
+        if op != '_div_':
+            raise
+        nn = QuaLiKizComboNN(pd.Series(target),
+                             [networks.pop(target_name), networks[leading]],
+                             lambda x, y: x * y)
+        networks[target] = nn
+
+gam = networks.pop('gam_leq_GB')
+nets = list(networks.values())
+combo_target_names = list(networks.keys())
 
 combo_nn = QuaLiKizComboNN(pd.Series(combo_target_names), nets, combo_func)
 vic_nn = VictorNN(combo_nn, gam)
